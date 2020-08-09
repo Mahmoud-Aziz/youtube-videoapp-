facf7f5900f866551cce0d0142408e70d4e00692
mahmoud <mahmoudaziz@Mahmouds-MacBook-Pro.local>
2020-08-09 14:57:27 +0200

    initial xcode project creation

---
+++ youtubeVideoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubeVideoApp
+//
+//  Created by Mahmoud Aziz on 8/9/20.
+//  Copyright © 2020 Mahmoud Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
